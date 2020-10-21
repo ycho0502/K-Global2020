66ef8fb221900351dfe5b082bbfe5c9213c4092e
ray <raychung0520@gmail.com>
2020-10-21 16:46:51 -0700

    HTML,CSS,JS: Finished navbar applying transparancy w/ JS

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+// Navbar Transparency
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  console.log(`navbarHeight: $(navbarHeight)`);
+  if(window.scrollY > navbarHeight){
+    navbar.classList.add('navbar--dark');
+  }else{
+    navbar.classList.remove('navbar--dark');
+  }
+});
