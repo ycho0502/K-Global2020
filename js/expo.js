3bdbddb676ed96f5b5d565447632f4d1f4ac7ae2
Yunho Cho <ycho0502@gmail.com>
2020-10-24 02:38:28 -0700

    companies are categorized and sorted, displaying company cards my category section

--- js/expo.js
+++ js/expo.js
@@ -1,26 +1,138 @@
+// Init. category element
+const smartAppliancesList = document.getElementById(
+  "cardList__smartAppliances"
+);
+const telecomunicateList = document.getElementById(
+  "cardList__telecomunications"
+);
+const biotechList = document.getElementById("cardList__biotech");
+const securityList = document.getElementById("cardList__security");
+const adasList = document.getElementById("cardList__adas");
+const roboticsList = document.getElementById("cardList__robotics");
+const itServiceList = document.getElementById("cardList__itServices");
+
+// Init etc
 const overlay = document.getElementById("overlay");
 const panelLogo = document.querySelector(".partnerPanel__logo");
 const partnerPanel = document.querySelector(".partnerPanel");
 const closeBtn = document.querySelector(".partnerPanel__closeBtn");
 
+// Company by category
+const smartAppliances = [];
+const telecomunicate = [];
+const biotech = [];
+const security = [];
+const adas = [];
+const robotics = [];
+const itService = [];
+//===================================================================================================
+
 //Fetching company data from json file
 //Usually gives error unless json is provided from the server
 fetch("data/companies.json")
   .then((response) => response.json())
-  .then((companies) => renderCompanies(companies))
+  .then((companies) => {
+    //Categorize
+    categorizeCompany(companies);
+
+    //Alphabetize
+    sortCompany();
+
+    renderCompanies();
+  })
   .catch((err) => {
     console.error("Error fetching data");
     console.error(err);
   });
 
-// Populating company cards
-function renderCompanies(companies) {
+//===================================================================================================
+
+function categorizeCompany(companies) {
+  const SMARTAPPLIANCES = "smart appliances";
+  const TELECOMMUNICATE = "telecommunications";
+  const BIOTECT = "biotech";
+  const SECURITY = "security";
+  const ADAS = "adas";
+  const ROBOTICS = "robotics";
+  const ITSERVICES = "it services";
+
   let company = companies.company;
-  const cards = document.querySelector(".container__cardList");
 
   company.forEach((item) => {
+    if (item.category === SMARTAPPLIANCES) {
+      smartAppliances.push(item);
+    } else if (item.category === TELECOMMUNICATE) {
+      telecomunicate.push(item);
+    } else if (item.category === BIOTECT) {
+      biotech.push(item);
+    } else if (item.category === SECURITY) {
+      security.push(item);
+    } else if (item.category === ADAS) {
+      adas.push(item);
+    } else if (item.category === ROBOTICS) {
+      robotics.push(item);
+    } else if (item.category === ITSERVICES) {
+      itService.push(item);
+    }
+  });
+}
+function sortCompany() {
+  smartAppliances.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+  telecomunicate.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+  biotech.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+  security.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+  adas.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+  robotics.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+  itService.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+}
+// Populating company cards
+function renderCompanies() {
+  // const biotech = biotechList
+  // const security = securityList
+  // const adas = adasList
+  // const robotics = roboticsList
+  // const itService = itServiceList
+  smartAppliances.forEach((item) => {
     let card = cardCreate(item);
-    cards.appendChild(card);
+    smartAppliancesList.appendChild(card);
+  });
+  telecomunicate.forEach((item) => {
+    let card = cardCreate(item);
+    telecomunicateList.appendChild(card);
+  });
+  biotech.forEach((item) => {
+    let card = cardCreate(item);
+    biotechList.appendChild(card);
+  });
+  security.forEach((item) => {
+    let card = cardCreate(item);
+    securityList.appendChild(card);
+  });
+  adas.forEach((item) => {
+    let card = cardCreate(item);
+    adasList.appendChild(card);
+  });
+  robotics.forEach((item) => {
+    let card = cardCreate(item);
+    roboticsList.appendChild(card);
+  });
+  itService.forEach((item) => {
+    let card = cardCreate(item);
+    itServiceList.appendChild(card);
   });
 }
 
