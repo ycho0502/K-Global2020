7685559215e3ef8b7a5e79d36a37b7ab9f5b63df
ray <raychung0520@gmail.com>
2020-10-29 11:56:32 -0700

    HTML,CSS: Finished a responsive page of introduction

--- js/main.js
+++ js/main.js
@@ -5,11 +5,34 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(`navbarHeight: $(navbarHeight)`);
+  // console.log(window.scrollY);
+  // console.log(`navbarHeight: $(navbarHeight)`);
   if(window.scrollY > navbarHeight){
     navbar.classList.add('navbar--dark');
   }else{
     navbar.classList.remove('navbar--dark');
   }
+});
+
+//Toggle hamburger menu
+
+const navbarMenu = document.querySelector('.navbar__menu');
+
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+
+navbarToggleBtn.addEventListener('click', ()=> {
+  console.log('ttt');
+  console.log(navbarMenu);
+  navbarMenu.classList.toggle('open');
+
+
+});
+
+
+const subMenu = document.querySelector('.sub-menu');
+const subMenuList = document.querySelector('.sub-menu__list');
+
+subMenu.addEventListener('click', ()=>{
+
+  subMenuList.classList.toggle('open');
 });
